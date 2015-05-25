41bda1dbad4fe3939625116530df67757ddba799
linsj <lin.sujie1234@gmail.com>
2015-05-25 23:12:40 +0200

    add the thread pool.rb for ruby thread

--- rakeutils/dependency.rb
+++ rakeutils/dependency.rb
@@ -5,10 +5,19 @@ class Depend
 
   def initialize(hash, order = nil)
     @dirs, @lib, @inc = [], [], []
+    append(hash, order)
+  end
+
+  def append(hash, order = nil)
     readhash(hash)
-    ltmp = @lib.transpose
-    @lib_to_s = (ltmp[0] + sort(ltmp[1].flatten, order)).join(' ')
+
+    sort(order)
     @inc_to_s = @inc.join(' ')
+  end
+
+  def sort(order)
+    ltmp = @lib.transpose
+    @lib_to_s = (ltmp[0] + sort_vec(ltmp[1].flatten, order)).join(' ')
   end
 
   private
@@ -22,15 +31,20 @@ class Depend
     end
   end
 
-  def sort(vector, order)
+  def sort_vec(vector, order)
     return vector unless order
     orderl = order.map { |x| "-l#{x}" }
     orderl + (vector - orderl)
   end
 
+  def unexist?(x)
+    return true if @lib.empty?
+    @lib.transpose[1].flatten.include?("-l#{x}") ? nil : true
+  end
+
   def record(path, libs)
     @dirs << path
-    @lib << ["-L#{path}/lib", libs.map { |x| "-l#{x}" }]
+    @lib << ["-L#{path}/lib", libs.map { |x| "-l#{x}" if unexist?(x) }.compact]
     @inc << "-I#{path}/include"
   end
 end
