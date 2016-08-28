911f431503310997633b2bfcf21cbdb01857ad38
linsj <lin.sujie1234@gmail.com>
2016-08-28 20:51:06 +0800

    root_convert.C should not contain '-' in name

--- nethack.sh
+++ nethack.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #This is a script for nethack
+DIR='/usr/games/'
 if [[ $1 == 0 ]]; then
   #This script is used to save the nethack game(Forgive my breaking the principle of roguelike games)
-  DIR='/usr/games/bin/'
-  SNAME=$HOME/.nethack/save
-  BNAME=$HOME/.nethack/bak
-  if [ `ls $DIR/save | wc -w` == '0' ]; then
-    sudo ln -f $DIR/bak/$BNAME $DIR/save/$BNAME
-    echo 'The file is recovered'
-  else
+  SNAME=$DIR/lib/nethackdir/save/1000linsj.gz
+  BNAME=$DIR/lib/nethackdir/backup/1000linsj.gz
+  if [[ -e $SNAME ]]; then
     echo 'The file is back up'
-    sudo ln -f $DIR/save/$SNAME $DIR/bak/$SNAME
+    sudo ln -f $SNAME $BNAME
+  else
+    sudo ln -f $BNAME $SNAME
+    echo 'The file is recovered'
   fi
 else
   #This is to open the nethack in a right way
+  echo $DIR/nethack
   $DIR/nethack
 fi
